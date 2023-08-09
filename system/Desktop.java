a30482309cc6666fef60ad17b587169c0768d9c4
ananthim2003 <139789172+ananthim2003@users.noreply.github.com>
2023-08-09 12:01:59 -0500

    Assignment1

---
+++ system/Desktop.java
@@ -0,0 +1,16 @@
+package org.system;
+
+public class Desktop extends Computer {
+	public void desktopSize() {
+		System.out.println("Desktop size:6.03");
+	}
+	 
+public static void main(String[] args) {
+		Desktop sys=new Desktop();
+		sys.computerModel();
+		sys.desktopSize();
+		
+
+	}
+
+}
